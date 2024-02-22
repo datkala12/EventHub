3e052fc4e246622707cdfa54cd99a5f2feb97980
Thành Đạt <103475806+datkala12@users.noreply.github.com>
2024-02-22 10:23:40 +0700

    add src

---
+++ src/main/java/com/evnt/ol/EvthApplication.java
@@ -0,0 +1,13 @@
+package com.evnt.ol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvthApplication.class, args);
+	}
+
+}
